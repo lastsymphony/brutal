d8c8a2a23ef73d8062752ad7597ac9c08f84516e
lastsymphony <lastsymphony01@gmail.com>
2025-12-15 18:10:08 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: 'https://yourusername.github.io/brutalist-astro',
-  build: {
-    format: 'static',
-    trailingSlash: 'always'
-  },
-  integrations: []
+  site: "https://lastsymphony.github.io",
+  base: "/brutal/",
+  output: "static",
 });
